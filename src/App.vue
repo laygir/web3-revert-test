65fa8813473e8cb7e98d9992d183f3c6bf4f9b63
Ozgur Atmaca <ozguratmaca@gmail.com>
2022-02-22 00:59:10 +0100

    uses alchemy

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
   <div id="app">
-    <button @click="connect">Connect</button>
+    <button @click="connect">Connect using Alchemy</button>
   </div>
 </template>
 
 <script>
-import Web3 from "web3";
+// import Web3 from "web3";
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+
 import CallRevert from "./CallRevert.json";
 
-const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+// const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+const web3 = createAlchemyWeb3(process.env.VUE_APP_ALCHEMY_KEY);
+web3.eth.handleRevert = true;
 
 export default {
   name: "App",
+  created() {
+    if (typeof window.ethereum !== "undefined") {
+      console.log("MetaMask is installed!");
+    } else {
+      console.log("MetaMask is NOT installed!");
+    }
+  },
   methods: {
     async connect() {
       try {
-        const account = web3.eth.accounts.create("my-pass-phrase");
-        web3.eth.accounts.wallet.add(account);
         const accounts = await web3.eth.getAccounts();
 
-        await web3.eth.sendTransaction({
-          from: accounts[0],
-          to: account.address,
-          value: web3.utils.toWei("0.01", "ether"),
-        });
-
-        console.log(
-          "%%%%% account balance: ",
-          await web3.eth.getBalance(account.address)
-        );
+        console.log("accounts", accounts);
 
         const contract = new web3.eth.Contract(CallRevert.abi, {
           data: CallRevert.bytecode,
@@ -39,7 +39,7 @@ export default {
             arguments: [],
           })
           .send({
-            from: account.address,
+            from: accounts[0],
             gas: 2850000,
           });
 
